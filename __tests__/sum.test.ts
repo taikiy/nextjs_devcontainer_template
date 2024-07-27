7b724e8c55c82775c33815b168230b8582f37a99
Taiki Yamaguchi <taiki.yamaguchi@gmail.com>
2024-07-27 18:11:27 +0900

    enable lint and pre-hooks

---
+++ __tests__/sum.test.ts
@@ -0,0 +1,5 @@
+describe("test", () => {
+  test("test 1 plus 1", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
